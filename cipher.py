a42fa71e11bb0a995e14cbbae6cbb0bf3de6624c
Courtney Edwards <courtneyee08@live.com>
2024-11-16 13:16:33 -0500

    Adding code for Casear Cipher Challenge

--- cipher.py
+++ cipher.py
@@ -1,1 +1,18 @@
 # add your code here
+alphabet = " abcdefghijklmnopqrstuvwxyz"
+
+sentence = str(input("Enter the sentence you wish to decipher: "))
+sentence = sentence.lower()
+shift = 5
+secret_sent = ""
+ 
+for char in sentence:
+  if char == " ":
+    secret_sent += " " 
+  elif char in alphabet:
+    index = alphabet.find(char)
+    index = (index + shift) % 26
+    char = alphabet[index]
+    secret_sent += char
+
+print(secret_sent)
